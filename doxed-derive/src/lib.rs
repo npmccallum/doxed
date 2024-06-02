fc945df6506bc83ef9b4dfa965bd091eb2c346a6
Nathaniel McCallum <nathaniel@mccallum.life>
2024-06-01 18:46:34 -0700

    Clean up the derive macro
    Signed-off-by: Nathaniel McCallum <nathaniel@mccallum.life>

--- doxed-derive/src/lib.rs
+++ doxed-derive/src/lib.rs
@@ -3,58 +3,43 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, Attribute, DeriveInput, Expr, ExprLit, Lit, Meta};
+use syn::{Attribute, DeriveInput, Error, Expr, ExprLit, Lit, Meta};
 
 const UNSUPPORTED: &str = r#"only `/// ...` or `#[doc = "..."]` are supported"#;
-const MISSING: &str = r#"missing doc attribute(s)"#;
 
-#[proc_macro_derive(Doxed)]
-pub fn derive_docs(input: TokenStream) -> TokenStream {
-    // Parse the input.
-    let input = parse_macro_input!(input as DeriveInput);
+fn filter_attr(attr: &Attribute) -> Option<Result<String, Error>> {
+    match &attr.meta {
+        Meta::NameValue(nv) if nv.path.is_ident("doc") => match &nv.value {
+            Expr::Lit(ExprLit {
+                lit: Lit::Str(s), ..
+            }) => Some(Ok(s.value())),
+            _ => Some(Err(Error::new_spanned(attr, UNSUPPORTED))),
+        },
+        _ => None,
+    }
+}
 
-    // Get the type name and generics.
+fn inner(input: DeriveInput) -> Result<TokenStream, Error> {
+    // Extract all the doc attributes from the input.
+    let dox = input
+        .attrs
+        .iter()
+        .filter_map(filter_attr)
+        .collect::<Result<Vec<String>, _>>()?;
+
+    // Generate the implementation.
     let name = &input.ident;
     let generics = &input.generics;
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-
-    // Extract all the doc attributes from the input.
-    let result: Result<Vec<_>, _> = input
-        .attrs
-        .iter()
-        .filter_map(|attr| match attr {
-            Attribute {
-                meta: Meta::NameValue(nv),
-                ..
-            } if nv.path.is_ident("doc") => match &nv.value {
-                Expr::Lit(ExprLit {
-                    lit: Lit::Str(s), ..
-                }) => Some(Ok(s.value())),
-                _ => Some(Err(UNSUPPORTED)),
-            },
-            _ => None,
-        })
-        .collect();
-
-    let error = match result {
-        // Handle error cases.
-        Ok(dox) if dox.is_empty() => MISSING,
-        Err(err) => err,
-
-        // Generate the impl block.
-        Ok(dox) => {
-            let expanded = quote! {
-                impl #impl_generics Doxed for #name #ty_generics #where_clause {
-                    const DOX: &'static [&'static str] = &[#(#dox),*];
-                }
-            };
-
-            return expanded.into();
+    Ok(From::from(quote::quote! {
+        impl #impl_generics Doxed for #name #ty_generics #where_clause {
+            const DOX: &'static [&'static str] = &[#(#dox),*];
         }
-    };
+    }))
+}
 
-    syn::Error::new_spanned(input.ident, error)
-        .to_compile_error()
-        .into()
+#[proc_macro_derive(Doxed)]
+pub fn derive_dox(input: TokenStream) -> TokenStream {
+    inner(syn::parse_macro_input!(input as DeriveInput))
+        .unwrap_or_else(|err| err.to_compile_error().into())
 }
